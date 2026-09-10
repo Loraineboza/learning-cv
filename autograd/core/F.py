7c8de26e0778c28f3bba3ed828b9f7671a43da4b
Loraineboza <pauldanilchenko2000@gmail.com>
2026-09-10 21:30:10 +0300

    fixed tensor. normalized diff

--- autograd/core/F.py
+++ autograd/core/F.py
@@ -7,28 +7,44 @@ class Binary_cross_entropy:
     def bce(self, pred, true):
         pred = np.array(pred)
         true = np.array(true)
-        eps = 1e-15 #эпсилион -число с 15 нулями впереди
-        pred = np.clip(pred, eps, 1-eps) #Если pred < eps: pred=eps; иначе если pred > 1-eps: pred=1-eps
+        eps = 1e-15
+        pred = np.clip(pred, eps, 1 - eps)
         loss = -np.mean(true * np.log(pred) + (1 - true) * np.log(1 - pred))
-
         self.pred, self.true = (pred, true)
         return loss
 
     def backward(self):
-        prod1 = self.true * 1/self.pred
-        prod2 = (1-self.true) * (1/1-self.pred) * (-1)
+        prod1 = self.true * 1 / self.pred
+        prod2 = (1 - self.true) * (1 / (1 - self.pred)) * (-1)
         dl_dz = -(prod1 + prod2)
-
-
         return dl_dz / len(self.true)
 
 class MSE:
     def __init__(self):
         pass
+
     def mse(self, pred, true):
         self.pred, self.true = (pred, true)
         return np.mean((pred - true) ** 2)
 
     def mse_grad(self):
-        dl_dz = 2 * (self.pred - self.true) 
+        dl_dz = 2 * (self.pred - self.true)
         return dl_dz / len(self.true)
+
+
+def MSE(y_pred, y_true):
+    diff = y_pred - y_true
+    sq = diff ** 2
+    loss = sq.mean()
+    loss._mse_cache = (y_pred, y_true)
+    return loss
+
+
+def Binary_cross_entropy_loss(y_pred, y_true):
+    eps = 1e-15
+    y_pred_clipped = y_pred.clip(eps, 1 - eps)
+    loss_term1 = y_true * y_pred_clipped.log()
+    loss_term2 = (1 - y_true) * (1 - y_pred_clipped).log()
+    loss = -(loss_term1 + loss_term2).mean()
+    loss._bce_cache = (y_pred, y_true)
+    return loss
