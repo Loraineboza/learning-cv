18529fe493eaf981c66545fc2582ca3963bcd4da
Loraineboza <pauldanilchenko2000@gmail.com>
2026-09-11 12:38:28 +0300

    final core

--- autograd/core/autograd.py
+++ autograd/core/autograd.py
@@ -280,17 +280,4 @@ class Tensor:
 
 
 
-# test
-X = Tensor(np.array([[1.0, 2.0],
-                     [2.0, 1.0],
-                     [3.0, 0.5]]), name="X")
-W = Tensor(np.random.randn(2, 1), name="W")
-b = Tensor(np.random.randn(1), name="b")
-y_pred = (X @ W + b).sum(axis=1, keepdims=True)
-y_true = Tensor(np.array([[1.0], [0.0], [1.0]]), name="y_true")
-loss = MSE(y_pred, y_true)
-loss.backward()
 
-print("Loss:", loss.data)
-print("W.grad:", W.grad)
-print("b.grad:", b.grad)
